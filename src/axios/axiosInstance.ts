6770664027082adfd7a850deb595b41c5315aa8f
Daniel Mihai <danielmihai@daniels-laptop.home>
2024-04-28 11:07:13 +0100

    testing frontend

--- src/axios/axiosInstance.ts
+++ src/axios/axiosInstance.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 
 
+// Added server 
 export const axiosInstance = axios.create({
     baseURL: process.env.NODE_ENV === 'production' ? 'https://blog-server-bae4.onrender.com/api' : 'http://localhost:3000/api',
     withCredentials: true
